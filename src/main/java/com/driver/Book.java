ed84821bf682f79cc464d1a038d3518d472bb869
MRITUNJAY <mritunjaykumartiwari1@gmail.com>
2023-06-27 23:25:42 +0530

    solution

--- src/main/java/com/driver/Book.java
+++ src/main/java/com/driver/Book.java
@@ -1,7 +1,13 @@
 package com.driver;
-
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+@Entity
 public class Book {
-    
+
+     @Id
+     @GeneratedValue(strategy = GenerationType.IDENTITY)
      private int id;
 
      private String name;
@@ -12,6 +18,12 @@ public class Book {
 
      public Book(){
 
+     }
+
+     public Book(String name, String genre, String author) {
+          this.name = name;
+          this.genre = genre;
+          this.author = author;
      }
 
      public int getId() {
--- src/main/java/com/driver/BookController.java
+++ src/main/java/com/driver/BookController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("books")
+@RequestMapping("/books")
 public class BookController {
     @Autowired
     BookService bookService;
